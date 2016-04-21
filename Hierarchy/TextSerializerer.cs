5717ef05d19ec87ddbccc94eaadec76ae061f5c2
Dmitry <mr.blacky01@gmail.com>
2016-04-21 11:39:52 +0300

    Reaslize Deserialize Method

--- Hierarchy/TextSerializerer.cs
+++ Hierarchy/TextSerializerer.cs
@@ -14,16 +14,34 @@ namespace Hierarchy
             List<Drinks> list = element as List<Drinks>;
             foreach (Drinks el in list)
             {
-                //byte[] array = System.Text.Encoding.Default.GetBytes(el.ToText()+"\n");
-                // запись массива байтов в файл
                 byte[] array = Encoding.UTF8.GetBytes(el.ToText() + "\r\n");
                 stream.Write(array, 0, array.Length);
             }
         }
 
-        public void Deserialize(Stream stream)
+        public virtual object Deserialize(Stream stream,object element)
         {
+            byte[] array = new byte[stream.Length];
+            stream.Read(array, 0, array.Length);
+            string textFromFile = System.Text.Encoding.Default.GetString(array);
+            string[] lines = textFromFile.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            List<Drinks> temp = new List<Drinks>();
+            var DeserializersList = new DrinkDeserializersList();
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(new char[] { ' ', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
 
+                foreach(TextDeserializer e in DeserializersList)
+                {
+                    if (e.Name == words[0])
+                    {
+                        temp.Add(e.Deserialize(words));
+                        break;
+                    }
+                }
+            }
+
+            return temp;
         }
     }
 }

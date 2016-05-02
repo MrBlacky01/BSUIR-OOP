c6dcfa3e47199f15f92030495a0ede764f9adccf
Dmitry <mr.blacky01@gmail.com>
2016-05-02 19:43:24 +0300

    Add Archivation Interface

---
+++ Hierarchy/InterfacePlugin/InterfacePlugin/InterfacePlugin.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace InterfacePlugin
+{
+    public interface IPlugin
+    {
+        void Archivate(Stream stream, string path);
+        void Dearchivate(Stream stream);
+        string Name { get; }
+        string Exe { get; }
+    }
+}

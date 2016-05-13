1238a38f391506849ae7e5ad90a18f16de9c1ae1
Dmitry <mr.blacky01@gmail.com>
2016-05-13 23:02:43 +0300

    Change dictionary ro make fitch find files for extensions and serialize

--- Hierarchy/Serializers/DictionaryForSerialize.cs
+++ Hierarchy/Serializers/DictionaryForSerialize.cs
@@ -8,14 +8,14 @@ namespace Hierarchy
 {
     class DictionaryForSerialize
     {
-        public Dictionary<int,ISerializable> dict { get;private set; }
+        public Dictionary<string,ISerializable> dict { get;private set; }
 
         public DictionaryForSerialize ()
         {
-            dict = new Dictionary<int, ISerializable>();
-            dict.Add(1, new XMLSerializerer());
-            dict.Add(2, new BinarySerializerer());
-            dict.Add(3, new TextSerializerer());
+            dict = new Dictionary<string, ISerializable>();
+            dict.Add("xml", new XMLSerializerer());
+            dict.Add("dat", new BinarySerializerer());
+            dict.Add("txt", new TextSerializerer());
 
         }
     }
